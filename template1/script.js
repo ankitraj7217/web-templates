3c32e6070148ce551520e230558c8818afeddcb4
Ankit Raj <ankitraj7217@gmail.com>
2024-04-11 23:14:13 +0530

    Update script.js: template1

--- template1/script.js
+++ template1/script.js
@@ -1,15 +1,1 @@
-const txt = document.querySelector(".home-content-role");
 
-const differentRoles = () => {
-    const roles = ["Software Engineer", "Otaku", "Traveller", "IR Enthusiast"];
-    let index = 0;
-    txt.textContent = roles[index];
-    index += 1
-
-    setInterval(() => {
-        txt.textContent = roles[index];
-        index = (index + 1) % roles.length; // Move to the next role, looping back to the beginning if needed
-    }, 4000); // Change every 4 seconds (4000 milliseconds)
-}
-
-differentRoles();
